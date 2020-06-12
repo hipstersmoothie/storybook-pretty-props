b2ac48747e52371bd1bd0b72fac74403fa2f657d
Andrew Lisowski <lisowski54@gmail.com>
2020-06-12 11:39:52 -0700

    remove prettier as a runtime dep

--- src/types/OneOf.tsx
+++ src/types/OneOf.tsx
@@ -1,42 +1,37 @@
 import React from "react";
-import prettier from "prettier/standalone";
-import parserTypescript from "prettier/parser-typescript";
 import { getPropTypes, PropRenderer, EnumType } from "./types";
 import { joinValues, HighlightButton } from "../utils";
 
-const MAX_LENGTH = 36;
+const MAX_LENGTH = 24;
+const ONE_OF = "oneOf = ";
 
 /** Render a oneOf type */
 const OneOf = ({ propType }: PropRenderer<EnumType>) => {
   const [minimized, setMinimized] = React.useState(true);
-  const didFormat = React.useRef(true);
   /** Toggle viewing the entire shape */
   const toggle = () => setMinimized(!minimized);
   const propTypes = getPropTypes(propType);
-  let code = `oneOf = ${
-    Array.isArray(propTypes) ? joinValues(propTypes) : propTypes
-  };`;
-
-  try {
-    code = prettier.format(code, {
-      parser: "typescript",
-      plugins: [parserTypescript],
-    });
-    didFormat.current = true;
-  } catch (e) {
-    didFormat.current = false;
-  }
+  const code = (
+    (Array.isArray(propTypes) &&
+      ((!minimized && `\n  ${joinValues(propTypes, " |\n  ")}`) ||
+        joinValues(propTypes))) ||
+    propTypes
+  ).toString();
 
   const isMinimizable = code.length > MAX_LENGTH;
   const typeDef = minimized ? code.substr(0, MAX_LENGTH) : code;
-  const isMultiline = !minimized && didFormat.current;
 
   return (
-    <span style={{ whiteSpace: isMultiline ? "pre" : "normal" }}>
-      {typeDef}
-      {isMinimizable && (
-        <HighlightButton onClick={toggle}>...</HighlightButton>
+    <span style={{ whiteSpace: minimized ? "normal" : "pre" }}>
+      {isMinimizable ? (
+        <HighlightButton style={{ fontSize: "0.9rem" }} onClick={toggle}>
+          {ONE_OF}
+        </HighlightButton>
+      ) : (
+        ONE_OF
       )}
+      {typeDef};
+      {isMinimizable && <HighlightButton onClick={toggle}>...</HighlightButton>}
     </span>
   );
 };
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import { PropTypeValue } from "./types/types";
-import { Element } from './types/types';
+import { Element } from "./types/types";
 
 /** Join a bunch of values in an or '|' sequence */
-export const joinValues = (propTypes: PropTypeValue[]) =>
-  propTypes.map(prop => ("value" in prop ? prop.value : prop.name)).join(" | ");
+export const joinValues = (propTypes: PropTypeValue[], delimiter = " | ") =>
+  propTypes
+    .map((prop) => ("value" in prop ? prop.value : prop.name))
+    .join(delimiter);
 
-  /** A button to show more or less of a shape type */
-export const HighlightButton = (props: Element<"button">) => (
+/** A button to show more or less of a shape type */
+export const HighlightButton = ({ style, ...props }: Element<"button">) => (
   <button
     type="button"
     {...props}
@@ -16,7 +18,8 @@ export const HighlightButton = (props: Element<"button">) => (
       background: "none",
       border: "0 none",
       color: "gray",
-      cursor: "pointer"
+      cursor: "pointer",
+      ...style,
     }}
   />
 );
