a9b07dc1e8131ab0f0ad447c7b8a7c89e5aa53bf
Andrew Lisowski <lisowski54@gmail.com>
2020-06-17 11:10:13 -0700

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import PrettyPropType from "./types/PrettyPropType";
 
-export { PrettyPropTypeProps } from "./types/PrettyPropType";
+export * from "./types/PrettyPropType";
 export * from "./types/types";
 
 export default PrettyPropType;
